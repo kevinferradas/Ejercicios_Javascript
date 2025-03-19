2444cb746405143028faf8d3528d8a708328831b
Kevin <kferradas@unitru.edu.pe>
2025-03-19 01:53:39 +0100

    avance 19/03

--- ex_per_practicar_03.js
+++ ex_per_practicar_03.js
@@ -3,3 +3,25 @@ let arrayMixto = [ "Marie", 24, "Pol", 18, "Judith", 22, "Eva", 28 ]
 
 // 9) Debes obtener otro array llamado arrayBidimensional que sea así­:
 // [ ["Marie", 24 ], ["Pol", 18], ["Judith", 22 ], [ "Eva", 28] ]
+
+let lista_temporal = [];
+
+let arrayBidimensional = [];
+
+for (elementos of arrayMixto) {
+    
+    while (lista_temporal.length<2){
+        lista_temporal.push(elementos)
+        if (lista_temporal.length==2){
+            arrayBidimensional.push(lista_temporal)
+            lista_temporal=[]
+        }
+        break
+    }
+   
+}
+
+
+ 
+lista_temporal
+arrayBidimensional
--- ex_per_practicar_04.js
+++ ex_per_practicar_04.js
@@ -3,7 +3,7 @@ const compra = [
     ["Leche", 1.2, 2],
     ["Pan", 0.8, 3],
     ["Huevos", 2.5, 1],
-    ["CafÃ©", 5.2, 0.5]
+    ["Café", 5.2, 0.5]
 ]
 // Se muestra el nombre del artí­culo, su precio y la cantidad comprada.
 // Debes obtener la cantidad de artículos comprados (no de cada tipo) y el importe total.
@@ -11,3 +11,13 @@ const compra = [
 
 // Después añade otro articulo al array anterior y muestra de nuevo el mensaje 
 // informativo con los nuevos datos.
+
+let importeTotal = 0
+let cantidadArticulos = 0
+for (listas of compra) {
+    importeTotal += listas.at(-2)*listas.at(-1);
+    cantidadArticulos += listas.at(-1);
+}
+
+console.log(`Has comprado ${cantidadArticulos} artículos y el importe total es de ${importeTotal} euros`);
+
